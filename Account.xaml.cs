bd128e601a9f278553d37a96171ae3053c0450a1
festep80 <74856291+festep80@users.noreply.github.com>
2021-12-15 21:49:20 +0200

    Initial commit

---
+++ Account.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Net;
+using System.Net.Mail;
+using MailKit.Net.Imap;
+using MimeKit;
+using System.Configuration;
+
+namespace EmailClient
+{
+    /// <summary>
+    /// Interaction logic for Account.xaml
+    /// </summary>
+    public partial class Account : Window
+    {
+        public Account()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            var login = tbLogin.Text;
+            var password = tbPassword.Text;
+
+            var client = new ImapClient();
+
+            client.Connect("imap.gmail.com", 993, true);
+            client.Authenticate(login, password);
+
+            if (!client.IsAuthenticated)
+            {
+                MessageBox.Show("Connection error!");
+                
+                return;
+            }
+            else
+            {
+                User user = new User();
+               
+                user.Login = login;
+                user.Client = client;
+                
+                
+            }
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,16 @@
+﻿using MailKit.Net.Imap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailClient
+{
+    public class User
+    {
+        public string Login { get; set; }
+        public ImapClient Client { get; set; }
+    }
+
+}
